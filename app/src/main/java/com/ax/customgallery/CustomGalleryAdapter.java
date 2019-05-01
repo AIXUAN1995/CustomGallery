5a397d834e84243be293c4bccf75ca9ae3ef7d05
aixuan <1822661289@qq.com>
2019-05-01 21:47:59 +0800

    滑动效果实现

--- app/src/main/java/com/ax/customgallery/CustomGalleryAdapter.java
+++ app/src/main/java/com/ax/customgallery/CustomGalleryAdapter.java
@@ -1,60 +1,72 @@
 package com.ax.customgallery;
 
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.ax.customgallery.utils.CollectionsUtils;
+import com.ax.customgallery.utils.DisplayUtils;
 import com.bumptech.glide.Glide;
 
 import java.util.List;
 
-import static android.support.constraint.Constraints.TAG;
-
 public class CustomGalleryAdapter extends RecyclerView.Adapter<CustomGalleryAdapter.ViewHolder> {
-	private List<GalleryItemBean> data;
-	private Context context;
+    private static final float SCALE = 0.85f;
+    private List<GalleryItemBean> data;
+    private Context context;
 
-	public CustomGalleryAdapter(List<GalleryItemBean> data, Context context) {
-		this.data = data;
-		this.context = context;
-	}
+    public CustomGalleryAdapter(List<GalleryItemBean> data, Context context) {
+        this.data = data;
+        this.context = context;
+    }
 
-	@Override
-	public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-		if (context == null) {
-			return null;
-		}
-		View view = LayoutInflater.from(context).inflate(R.layout.gallery_item, parent, false);
-		return new ViewHolder(view);
-	}
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (context == null) {
+            return null;
+        }
+        View view = LayoutInflater.from(context).inflate(R.layout.gallery_item, parent, false);
+        return new ViewHolder(view);
+    }
 
-	@Override
-	public void onBindViewHolder(ViewHolder holder, int position) {
-		if (context != null && holder != null && !CollectionsUtils.isEmptyList(data)) {
-			Glide.with(context)
-					.load(data.get(position).getImage())
-					.centerCrop()
-					.into(holder.imageView);
-			Log.i(TAG, "onBindViewHolder: " + data.get(position).getImage());
-		}
-	}
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        if (context == null || holder == null || CollectionsUtils.isEmptyList(data)) {
+            return;
+        }
+        ViewGroup.MarginLayoutParams lp = ((ViewGroup.MarginLayoutParams) holder.itemContent.getLayoutParams());
+        if (position == 0) {
+            lp.width = DisplayUtils.getItemWidth(context);
+            lp.height = DisplayUtils.getItemHeight(context);
+            holder.itemContent.setLayoutParams(lp);
+        } else {
+            lp.width = (int) (DisplayUtils.getItemWidth(context) * SCALE);
+            lp.height = (int) (DisplayUtils.getItemHeight(context) * SCALE);
+            holder.itemContent.setLayoutParams(lp);
+        }
+        Glide.with(context)
+                .load(data.get(position).getImage())
+                .centerCrop()
+                .into(holder.imageView);
+    }
 
-	@Override
-	public int getItemCount() {
-		return data == null ? 0 : data.size();
-	}
+    @Override
+    public int getItemCount() {
+        return data == null ? 0 : data.size();
+    }
 
-	static class ViewHolder extends RecyclerView.ViewHolder {
-		ImageView imageView;
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        CardView itemContent;
+        ImageView imageView;
 
-		public ViewHolder(View itemView) {
-			super(itemView);
-			imageView = itemView.findViewById(R.id.headView);
-		}
-	}
+        public ViewHolder(View itemView) {
+            super(itemView);
+            itemContent = itemView.findViewById(R.id.itemContent);
+            imageView = itemView.findViewById(R.id.headView);
+        }
+    }
 }
--- app/src/main/java/com/ax/customgallery/MainActivity.java
+++ app/src/main/java/com/ax/customgallery/MainActivity.java
@@ -5,78 +5,93 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ax.customgallery.utils.DisplayUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-	private String[] pics = {"http://gss0.baidu.com/7Po3dSag_xI4khGko9WTAnF6hhy/zhidao/pic/item/bd315c6034a85edfca479e4f48540923dc547596.jpg",
-			"http://ww2.sinaimg.cn/large/005ImClmjw1ern9ybe83yj30zk0k01cm.jpg",
-			"http://b-ssl.duitang.com/uploads/item/201802/13/20180213202427_vcykg.jpg",
-			"http://i0.hdslb.com/bfs/article/3de475241c65c721c242f4f3f854fbd115fc0dd0.jpg",
-			"http://b-ssl.duitang.com/uploads/item/201410/25/20141025181818_SSvPY.jpeg"};
-	private RecyclerView recyclerView;
-	private CustomGalleryAdapter adapter;
-	private String TAG = "MainActivity";
+    private String[] pics = {"http://gss0.baidu.com/7Po3dSag_xI4khGko9WTAnF6hhy/zhidao/pic/item/bd315c6034a85edfca479e4f48540923dc547596.jpg",
+            "http://ww2.sinaimg.cn/large/005ImClmjw1ern9ybe83yj30zk0k01cm.jpg",
+            "http://b-ssl.duitang.com/uploads/item/201802/13/20180213202427_vcykg.jpg",
+            "http://i0.hdslb.com/bfs/article/3de475241c65c721c242f4f3f854fbd115fc0dd0.jpg",
+            "http://b-ssl.duitang.com/uploads/item/201410/25/20141025181818_SSvPY.jpeg"};
+    private RecyclerView recyclerView;
+    private CustomGalleryAdapter adapter;
+    private String TAG = "MainActivity";
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		recyclerView = findViewById(R.id.recyclerView);
-		recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false));
-		adapter = new CustomGalleryAdapter(getData(), this);
-		recyclerView.setAdapter(adapter);
-		recyclerView.setOnFlingListener(new RecyclerView.OnFlingListener() {
-			@Override
-			public boolean onFling(int i, int i1) {
-				return true;
-			}
-		});
-		recyclerView.addOnScrollListener(new GalleryOnScrollListener());
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false));
+        adapter = new CustomGalleryAdapter(getData(), this);
+        recyclerView.setAdapter(adapter);
+        new CustomPagerSnapHelper().attachToRecyclerView(recyclerView);
+        recyclerView.addOnScrollListener(new GalleryOnScrollListener());
+    }
 
-	private List<GalleryItemBean> getData() {
-		List<GalleryItemBean> list = new ArrayList<>();
-		for (int i = 0; i < 5; i++) {
-			GalleryItemBean itemBean = new GalleryItemBean();
-			itemBean.setImage(pics[i]);
-			list.add(itemBean);
-		}
-		return list;
-	}
+    private List<GalleryItemBean> getData() {
+        List<GalleryItemBean> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            GalleryItemBean itemBean = new GalleryItemBean();
+            itemBean.setImage(pics[i]);
+            list.add(itemBean);
+        }
+        return list;
+    }
 
-	class GalleryOnScrollListener extends RecyclerView.OnScrollListener {
-		private int x = 0;//记录当前x轴坐标
-		private int y = 0;
-		private int scrollX = 0;//记录每次延x轴滚动的距离
-		private int scrollY = 0;
+    class GalleryOnScrollListener extends RecyclerView.OnScrollListener {
+        @Override
+        public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+            super.onScrollStateChanged(recyclerView, newState);
+        }
 
-		@Override
-		public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
-			super.onScrollStateChanged(recyclerView, newState);
-			Log.i(TAG, "newState:" + newState);
-			if (newState == 0) {//停止滚动
-				int dx;
-				Log.i(TAG, "scrollX:" + scrollX + "recyclerView.getWidth()" + recyclerView.getWidth());
-				if (scrollX > recyclerView.getWidth() / 2) {
-					dx = recyclerView.getWidth() - scrollX;
-				} else if (-scrollX > recyclerView.getWidth() / 2) {
-					dx = scrollX - recyclerView.getWidth();
-				} else {
-					dx = -scrollX;
-				}
-				Log.i(TAG, "dx:" + dx);
-				recyclerView.scrollBy(dx, 0);
-				scrollX = 0;
-			}
-		}
+        @Override
+        public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+            super.onScrolled(recyclerView, dx, dy);
+            if (recyclerView.getLayoutManager() instanceof LinearLayoutManager) {
+                slideInAnimation(recyclerView);
+            }
+        }
 
-		@Override
-		public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-			super.onScrolled(recyclerView, dx, dy);
-			scrollX += dx;
-		}
-	}
+        /**
+         * 当前第二页的动画
+         */
+        private void slideInAnimation(RecyclerView recyclerView) {
+            float percent = getScrollPercent(recyclerView);
+            if (percent > 0 && percent < 1) {
+                View targetView = getTargetView(recyclerView, 1);
+                if (targetView == null) {
+                    return;
+                }
+                ViewGroup.MarginLayoutParams lp = ((ViewGroup.MarginLayoutParams) targetView.getLayoutParams());
+                lp.width = (int) (DisplayUtils.getItemWidth(targetView.getContext()) * (0.85 + 0.15 * percent));
+                lp.height = (int) (DisplayUtils.getItemHeight(targetView.getContext()) * (0.85 + 0.15 * percent));
+                targetView.setLayoutParams(lp);
+            }
+        }
+
+        private float getScrollPercent(RecyclerView recyclerView) {
+            View firstItem = recyclerView.getChildAt(0);
+            if (firstItem == null) {
+                return 0;
+            }
+            LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+            int scrollDistance = firstItem.getWidth() - layoutManager.getDecoratedRight(firstItem);
+            return scrollDistance * 1.0f / firstItem.getWidth();
+        }
+
+        private View getTargetView(RecyclerView recyclerView, int index) {
+            View view = recyclerView.getChildAt(index);
+            if (view == null) {
+                return null;
+            }
+            return view.findViewById(R.id.itemContent);
+        }
+    }
 }
